1e0247d238e4dfe157b3a80889edb892ebb40766
garvit586 <guptagarvit009@gmail.com>
2024-12-28 22:17:34 +0530

    Chnage the course 8

--- 8.Data_visualisation/dash_plotly/create_project.py
+++ 8.Data_visualisation/dash_plotly/create_project.py
@@ -7,7 +7,15 @@ app = dash.Dash(__name__)
 
 # Define the layout of the app
 app.layout = html.Div(children=[
-    html.H1('XYZAutomotives: Historical Trends in Automobile Sales Analysis'),
+    html.H1(
+            "Automobile Sales Statistics Dashboard",
+            style={
+                "textAlign": "center",  # Center align the text
+                "color": "black",      # Text color
+                "fontSize": "36px",    # Font size
+                "marginTop": "20px",   # Add some margin
+            },
+        ),
     html.Div([
         html.P('This dashboard provides insights into how XYZAutomotives\' sales were impacted during recession periods.')
     ])
